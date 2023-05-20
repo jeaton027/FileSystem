32bf28fb34a96892b3231f59baed81f197625ad5
DaphneLoustalet <114203018+DaphneLoustalet@users.noreply.github.com>
2023-05-19 17:52:42 -0700

    Update fs.c
    Added comments and start Phase 1

--- libfs/fs.c
+++ libfs/fs.c
@@ -7,16 +7,47 @@
 #include "disk.h"
 #include "fs.h"
 
+// struct is a global variable so info can access
+struct superblock {
+        int64_t signature;
+        int16_t total_amt;
+        int16_t root_dir;
+        int16_t data_block;
+        int16_t block_amt;
+        int8_t block_num;
+        // type can change later this is just an array for excess
+        void* excess;
+};
 /* TODO: Phase 1 */
 
 int fs_mount(const char *diskname)
 {
-	/* TODO: Phase 1 */
+        /* TODO: Phase 1 */
+        // open the file on the disk
+        int return_val = block_disk_open(diskname);
+        // if output is -1 return -1
+        // otherwise continue getting info
+        if (return_val < 0) {
+                return -1;
+        }
+
+        // declare a void* buffer
+        // probably need to malloc the buffer and delete after
+        void* buffer;
+        block_read(0, buffer);
+        // will read data contents from superblock into buffer
+        // read contents from buffer into struct
+        // first 8 bytes read into entry 1
+        struct superblock* sb = malloc(struct superblock*)sizeof(struct superblock);
+        sb->excess = malloc(void*)sizeof(void);
 }
 
 int fs_umount(void)
 {
-	/* TODO: Phase 1 */
+        /* TODO: Phase 1 */
+        // delete everything stored in struct
+        // close
+        // block_disk_close()
 }
 
 int fs_info(void)
